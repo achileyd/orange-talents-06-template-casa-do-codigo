30f05a0c04667f9b91e237b09d2a2daaf7a697d5
achiley.pinto <achileyd@gmail.com>
2021-07-02 17:45:41 -0300

    Exibir lista de livros

--- src/main/java/br/com/zupacademy/achiley/casadocodigo/livro/LivroController.java
+++ src/main/java/br/com/zupacademy/achiley/casadocodigo/livro/LivroController.java
@@ -1,10 +1,13 @@
 package br.com.zupacademy.achiley.casadocodigo.livro;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,9 @@ public class LivroController {
 
 	@Autowired
 	private EntityManager manager;
+	
+	@Autowired
+	private LivroRepository livroRepository;
 
 	@PostMapping
 	@Transactional
@@ -25,5 +31,11 @@ public class LivroController {
 		
 		return livro.toString();
 		
+	}
+	
+	@GetMapping
+	public List<LivroDto> listar()  {
+		List<Livro> livros = livroRepository.findAll();
+		return LivroDto.converter(livros);
 	}
 }
--- src/main/java/br/com/zupacademy/achiley/casadocodigo/livro/LivroForm.java
+++ src/main/java/br/com/zupacademy/achiley/casadocodigo/livro/LivroForm.java
@@ -66,6 +66,7 @@ public class LivroForm {
 		this.autorId = autorId;
 	}
 	
+
 	public String getTitulo() {
 		return titulo;
 	}
